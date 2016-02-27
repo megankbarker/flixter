cf0d3121157fd85215808c8f1c95fd714acf68ff
rosebuddy <helloladybread@gmail.com>
2016-02-27 20:43:00 +0000

    List out courses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Flixter::Application.routes.draw do
   devise_for :users
   # devise_for :models
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end

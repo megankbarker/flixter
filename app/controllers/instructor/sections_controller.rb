c6ecae8b9e328b85c876514d38463c0e28d718f5
rosebuddy <helloladybread@gmail.com>
2016-02-28 05:42:33 +0000

    Fix sign-in form

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,9 @@
 class Instructor::SectionsController < ApplicationController
   def new 
     @course = Course.find(params[:course_id])
+  if@course.user != current_user
+    return render text: 'Unauthorized', status: unauthorized
+  end
     @section = Section.new
   end
 

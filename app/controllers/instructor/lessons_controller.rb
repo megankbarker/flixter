a3769888ab16951021e32d5e0a9374c8b4de9813
rosebuddy <helloladybread@gmail.com>
2016-02-28 07:39:20 +0000

    Complete lesson permissions

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,23 +1,29 @@
 class Instructor::LessonsController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
   def new
-    @section = Section.find(params[:section_id]) # need to load the section that is being referenced in the URL, by pulling the section_id from the param
-    if @section.course.user != current_user
-      return render text: 'Unauthorized', status: :unauthorized # By sending the unauthorized HTTP code, the browser becomes aware that the request was not successful and exits the function not executing line below
-    end
     @lesson = Lesson.new # before we add form to the view, we need to give the form a template object for the thing we're building the form for
   end
 
   def create 
-    @section = Section.find(params[:section_id]) # Look up the section in the database the lesson is for.
-    @lesson = @section.lessons.create(lesson_params) # Create the lesson in the database and connect it to the section we looked up.
-    redirect_to instructor_course_path(@section.course) #Redirect the user to the instructor dashboard.
-
+    @lesson = current_section.lessons.create(lesson_params) # Create the lesson in the database and connect it to the section we looked up.
+    redirect_to instructor_course_path(current_section.course) #Redirect the user to the instructor dashboard.
   end
 
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render text: 'Unauthorized', status: :unauthorized # By sending the unauthorized HTTP code, the browser becomes aware that the request was not successful and exits the function not executing line below
+    end
+  end
+
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)

3138a9269cd203f7caf7e7e20ae6833d4f299f68
rosebuddy <helloladybread@gmail.com>
2016-03-02 08:13:40 +0000

    Reorder sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
   before_action :require_authorized_for_current_section, only: [:update]
 
   def new 
@@ -12,22 +12,35 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course) # Redirect the user to the corresponding instructor show course page
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'updated!'
+  end
+
+  private
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   def require_authorized_for_current_course
     if current_course.user != current_user
       render text: "Unauthorized", status: :unauthorized
     end
   end
 
-  private
 
   helper_method :current_course
   def current_course
+    if params[:course_id]
     @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
   end
 
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
 
